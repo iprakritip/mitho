6b8f8212eb0863a97e4f18db36d115fdd411f202
Prakriti Pokharel <iprakritip@gmail.com>
2023-03-01 16:02:14 +0545

    add an API to fetch the recipes
    the console inside the async function in Trending Component  isn't working

---
+++ src/components/Trending.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const Trending = () => {
+
+// const [trending, setTrending]= useState([]);
+
+const getTrending = async()=>{
+    const api = await fetch(
+    `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log("data");
+}
+
+useEffect=(()=>{
+    getTrending();
+  },[]);
+  return (
+    <div>Trending</div>
+  )
+}
+
+export default Trending
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Veg from "../components/Veg";
+import Trending from "../components/Trending";
+
+const Home = () => {
+  return (
+    <div>
+        <Veg />
+        <Trending />
+    </div>
+  )
+}
+
+export default Home
